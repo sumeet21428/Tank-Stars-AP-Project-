fa51fd81520549edc8f6a77e2d56d061728885fd
sumeet21428 <sumeet21428@iiitd.ac.in>
2022-12-21 17:20:56 +0530

    few tweaks

--- core/src/com/approject/game/States/PlayState.java
+++ core/src/com/approject/game/States/PlayState.java
@@ -1,10 +1,15 @@
 package com.approject.game.States;
 
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class PlayState extends State{
+    private Texture tank;
+
+
     public PlayState(GameStateManager gsm) {
         super(gsm);
+        tank = new Texture("tank.png");
     }
 
     @Override
@@ -19,6 +24,9 @@ public class PlayState extends State{
 
     @Override
     public void render(SpriteBatch sb) {
+        sb.begin();
+        sb.draw(tank, 10, 85);
+        sb.end();
 
     }
 
